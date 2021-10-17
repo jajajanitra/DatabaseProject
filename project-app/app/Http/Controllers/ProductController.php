1b1b1a6fb9298e02145b022be7861fc3bc8ac494
janitra <jajajanitra@gmail.com>
2021-10-18 00:38:52 +0700

    add choices of productline to form

--- project-app/app/Http/Controllers/ProductController.php
+++ project-app/app/Http/Controllers/ProductController.php
@@ -41,6 +41,7 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         //
+        $productlines = Productline::all();
         $product = new Product([
             'productCode' => $request->productCode,
             'productName' => $request->productName,
