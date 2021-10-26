e191b2375cb3d4646992e2e0629c91ba91533581
janitra <jajajanitra@gmail.com>
2021-10-27 01:59:51 +0700

    fix

--- project-app/resources/views/cart.blade.php
+++ project-app/resources/views/cart.blade.php
@@ -15,12 +15,13 @@
 
         <?php $total = 0 ?>
 <!-- by this code session get all product that user chose -->
-    
+        <?php $k?>
+        <?php $i=0?>
         @if(session('cart'))
             @foreach(session('cart') as $id => $j)
 
                 <?php $total += $j['buyPrice'] * $j['quantity'] ?>
-
+                <?php $i+=1?>
                 <tr>
                     <td data-th="Product">
                         <div class="row">
@@ -49,6 +50,7 @@
                     </form>
                     </td>
                 </tr>
+                <?php $k[$i]=[$j['productCode'],$j['buyPrice'],$j['quantity'],$j['productName'],$j['productLine'],$j['quantityInStock'],$j['productScale'],$j['productVendor'],$j['productDescription'],$j['MSRP'],$j['productStatus']]?>
             @endforeach
         @endif
         <tr>
@@ -57,64 +59,81 @@
             <td class="hidden-xs text-center"><strong>Total ${{ $total }}</strong></td>
         </tr>
         </tbody>
+        <div>
         <tfoot>
             
             <form action="{{url('/products/cart')}}" method="post">
-            @csrf
-            @method('POST')
-                <div class="form-group">
-                    <label>Customer Number :</label>
-                    <input type="number" name="customerNumber" required>                              
-                </div>
-                <div>
-                    <label>order Date :</label>
-                    <input type="date" name="orderDate" id=e readonly>
-                    <script>document.getElementById('e').value = new Date().toISOString().substring(0, 10);</script>
-                </div>
-                <div>
-                    <label>requiredDate :</label>
-                    <input type="date" name="requiredDate" required>
-                </div>
-                <div>
-                    <label>shippedDate :</label>
-                    <input type="date" name="shippedDate" required>
-                </div>
-                <div>
-                    <label for="status">State</label>
-                        <select name="status"  >
-                            <option value="In Process">In Process</option> 
+            <div>
+                @csrf
+                @method('POST')
+                    <div class="form-group">
+                        <label>Customer Number :</label>
+                        <input type="number" name="customerNumber" required>                              
+                    </div>
+                    <div>
+                        <label>order Date :</label>
+                        <input type="date" name="orderDate" id=e readonly>
+                        <script>document.getElementById('e').value = new Date().toISOString().substring(0, 10);</script>
+                    </div>
+                    <div>
+                        <label>requiredDate :</label>
+                        <input type="date" name="requiredDate" required>
+                    </div>
+                    <div>
+                        <label>shippedDate :</label>
+                        <input type="date" name="shippedDate" required>
+                    </div>
+                    <div>
+                        <label for="status">State</label>
+                            <select name="status"  >
+                                <option value="In Process">In Process</option> 
+                            </select>
+                    </div>
+                    <div>
+                        <label>comment :</label>
+                        <input type="text" name="comments">
+                    </div>
+                    <div>
+                        <label>OrderType</label>
+                        <select name="orderType" required>
+                            <option value="normal">normal</option>
+                            <option value="preorder">preorder</option>
                         </select>
+                    <div>
+                        <label>couponNumber :</label>
+                        <input type="nymber" name="couponNumber" >
+                    </div>
+                    <button class="btn btn-info btn-sm update-total" 
+                    onclick="window.location.href=window.location.href"><i class="fa fa-refresh"></i></button>
+                    <div>
+                        <label>paymentNumber :</label>
+                        <input type="number" name="paymentNumber" required>
+                    </div>
+                    <div>
+                        <label>total : </label>
+                        <input  type="number" name="total" value="{{ $total }}" readonly>
+                    </div>
+                    <div>
+                    <?php $totalpoint =  floor($total/100) ?>
+                        <label>pointReceived : </label>
+                        <input  type="number" name="pointReceived" value="{{ $totalpoint }}" readonly>
+                    </div>
+                    @for ($p = 1; $p <= $i; $p++)
+                    <input type="text" name="productCode[]" value="{{$k[$p][0]}}">
+                    <input type="number" name="priceEach[]" value="{{$k[$p][1]}}">
+                    <input type="number" name="buyPrice[]" value="{{$k[$p][1]}}">
+                    <input type="number" name="quantityOrdered[]" value="{{$k[$p][2]}}">
+                    <input type="text" name="productName[]" value="{{$k[$p][3]}}">
+                    <input type="text" name="productLine[]" value="{{$k[$p][4]}}">
+                    <input type="number" name="quantityInStock[]" value="{{$k[$p][5]}}">
+                    <input type="text" name="productScale[]" value="{{$k[$p][6]}}">
+                    <input type="text" name="productVendor[]" value="{{$k[$p][7]}}">
+                    <input type="text" name="productDescription[]" value="{{$k[$p][8]}}">
+                    <input type="number" name="MSRP[]" value="{{$k[$p][9]}}">
+                    <input type="text" name="productStatus[]" value="{{$k[$p][10]}}">
+                    @endfor
+                    <button type="submit"> PlaceOrder </button>
                 </div>
-                <div>
-                    <label>comment :</label>
-                    <input type="text" name="comments">
-                </div>
-                <div>
-                    <label>OrderType</label>
-                    <select name="orderType" required>
-                        <option value="normal">normal</option>
-                        <option value="preorder">preorder</option>
-                    </select>
-                <div>
-                    <label>couponNumber :</label>
-                    <input type="nymber" name="couponNumber" >
-                </div>
-                <button class="btn btn-info btn-sm update-total" 
-                onclick="window.location.href=window.location.href"><i class="fa fa-refresh"></i></button>
-                <div>
-                    <label>paymentNumber :</label>
-                    <input type="number" name="paymentNumber" required>
-                </div>
-                <div>
-                    <label>total : </label>
-                    <input  type="number" name="total" value="{{ $total }}" readonly>
-                </div>
-                <div>
-                <?php $totalpoint =  floor($total/100) ?>
-                    <label>pointReceived : </label>
-                    <input  type="number" name="pointReceived" value="{{ $totalpoint }}" readonly>
-                </div>
-                <button type="submit"> PlaceOrder </button>
             </form>
         </tfoot>
     </table>
