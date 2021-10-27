542836579e05f8590c30a1f9452280a9515358f1
worsrp <sirapop_p@cmu.ac.th>
2021-10-27 23:58:37 +0700

    css

--- project-app/resources/views/layouts/store.blade.php
+++ project-app/resources/views/layouts/store.blade.php
@@ -20,10 +20,39 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/headerSty.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- cart extension-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div id="app" class="uni">
-    <header>
+    <header>    
+        <div>
+        @if(session('cart'))
+                <a href="{{ url('/products/cart') }}" class="cart">
+                    <!-- this code count product of choose tha user choose -->
+                    <span class="badge badge-pill badge-danger" style="position: absolute; top: 15px; right: 6px;">{{ count(session('cart')) }}</span>
+                </a>
+        </div>
+        <div class="col-sm-4 text-center">
+            @if(session('success'))
+            <p class="btn-success  mt-3 mb-3 btn-block session" style='padding: .375rem .75rem;'>{{ session('success') }}</p>
+        </div>
+            @endif
+            <!-- if user dont choose any product -->
+            @else
+            <a href="" class="btn text-light bg-warning mt-3 mb-3" role="button">
+                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                    Cart Empty
+            </a> 
+            @endif 
+        </div>
             <div>
             <div class="dropdown">
                 <a class="user" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="outline: none;">
@@ -38,7 +67,7 @@
             </form>
                 </div>
                 </div>
-            <div class="cart"></div>
+            <!-- <div class="cart"></div> -->
             <div class="circle"></div>
             <a class="store-text" href="{{ url('/') }}">{{ __('Store')}} </a>  
             
@@ -60,5 +89,22 @@
             @yield('content')
         </main>
     </div>
+    @yield('scripts')
+
+<script>
+    $("a").click(function () {
+        $(".session").visibility(2);
+        });
+        
+</script>
+
+<style>
+    .session{
+        visibility:hidden ;
+    }
+    body{
+        background-color: #fff;
+    }
+</style>
 </body>
 </html>
